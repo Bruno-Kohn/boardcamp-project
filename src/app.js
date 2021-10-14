beceac6668bcc1e88b9d3fbfca3a99d81e3fa03a
Bruno Kohn <brunokohn1993@gmail.com>
2021-10-14 10:37:19 -0300

    adicionando server

--- src/app.js
+++ src/app.js
@@ -1,12 +1,35 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import pg from "pg";
 
 const app = express();
+app.use(cors());
 
-app.get("/", (req,res) => {
-    res.send("Testando");
+//----- CONECTANDO O BANCO -----
+
+const { Pool } = pg;
+
+const connection = new Pool({
+  user: "bootcamp_role",
+  password: "senha_super_hiper_ultra_secreta_do_role_do_bootcamp",
+  host: "localhost",
+  port: 5432,
+  database: "boardcamp",
 });
 
+/*const query = connection.query("SELECT * FROM boardcamp");
+query.then((result) => {
+  console.log(result.rows);
+});*/
+
+//----- ROTA DE TESTE -----
+
+app.get("/", (req, res) => {
+  res.send("Testandoooooo");
+});
+
+//----- PORTA DO SERVIDOR -----
+
 app.listen(4000, () => {
-    console.log("Server running on port 4000!");
+  console.log("Server running on port 4000!!!!");
 });
